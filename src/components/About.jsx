5ab2ad7949c148984ce3609a463f1a2c0ee64f89
AjnasNB <20cs004ajna@ug.cusat.ac.in>
2023-05-02 16:21:03 +0530

    abt

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,7 +20,7 @@ const About = () => {
   return (
     <div className='stars'>
     
-    <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+    <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300 overflow-auto'>
       <div className='flex flex-col justify-center items-center w-full h-full'>
         <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
           <div className='sm:text-right pb-8 pl-4 mt-[50px] lg:mt-0'>
